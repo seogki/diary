262848bc7a7484a4421db499c97a7408c105df5b
shin <zun952@naver.com>
2021-03-09 15:41:41 +0900

    test

---
+++ server/src/App.ts
@@ -0,0 +1,18 @@
+import express from "express";
+
+class App{
+  public app: express.Application;
+
+  public static bootstrap(): App{
+    return new App();
+  }
+
+  constructor(){
+    this.app = express();
+    this.app.get("/", (req: express.Request, res: express.Response, next: express.NextFunction) => {
+      res.send("hello world");
+    });
+  }
+}
+
+export default App;
